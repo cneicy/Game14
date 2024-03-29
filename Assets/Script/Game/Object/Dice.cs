5c99e9f4b9152e40bd75462c0b17d5f7736ce854
cneicy <im@crash.work>
2024-03-29 13:42:48 +0800

    Game Design Document
    Signed-off-by: cneicy <im@crash.work>

--- Assets/Script/Game/Object/Dice.cs
+++ Assets/Script/Game/Object/Dice.cs
@@ -5,16 +5,36 @@ namespace Script.Game.Object
 {
     public class Dice : MonoBehaviour
     {
+        private TMP_Text _diceText;
+        private float RandomTime;
         private int Roll()
         {
             return Random.Range(1, 101);
+        }
+        public void Reset()
+        {
+            RandomTime = 0;
+        }
+        private bool Timer()
+        {
+            RandomTime += 4 * Time.deltaTime;
+            if (RandomTime >= 5)
+            {
+                return false;
+            }
 
+            return true;
         }
         private void Start()
         {
-            TMP_Text tMP_Text = GetComponent<TMP_Text>();
-            tMP_Text.text= Roll().ToString();
+            _diceText = GetComponent<TMP_Text>();
         }
-
+        private void Update()
+        {
+            if (Timer())
+            {
+                _diceText.text = Roll().ToString();
+            }
+        }
     }
 }
