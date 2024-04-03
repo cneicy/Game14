9b2b401e54e2ffb73d6e5dd8ca38febedb90e876
Eicy <im@crash.work>
2024-04-03 14:52:39 +0800

    Re

--- Assets/RollDiceButton.cs
+++ Assets/RollDiceButton.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
 
 public class RollDiceButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int[] arr={3,9,4,0,10};
+    private int[] bucket={0,0,0,0,0};
+    private void Start()
     {
-        
-    }
+        // int longOfArr = arr.Length;
+        // for (int i = 0; i < longOfArr; i++)
+        // {
+        //     for (int j = i+1; j < longOfArr; j++)
+        //     {
+        //         if (arr[i] > arr[j])
+        //         {
+        //             var temp = arr[i];
+        //             arr[i] = arr[j];
+        //             arr[j] = temp;
+        //         }
+        //     }
+        // }
+        // foreach (var VARIABLE in arr)
+        // {
+        //     print(VARIABLE);
+        // }
+        foreach (var VARIABLE in arr)
+        {
+            bucket[VARIABLE]++;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < bucket.Length; i++)
+        {
+            if (bucket[i] > 0)
+            {
+                print(i);
+                bucket[i]--;
+            }
+        }
     }
 }
